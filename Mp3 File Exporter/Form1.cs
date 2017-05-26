f5d264637dec4f1bb0664e8cd04ce3de3e2194dc
TheBicPen <mashkal2000@gmail.com>
2017-05-25 19:59:38 -0700

    Created basic program - currently only searches main folder and copies all files

---
+++ Mp3 File Exporter/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Mp3_File_Exporter
+{
+    public partial class Form1 : Form
+    {
+
+      string SourceFolder;
+      string DestinationFolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog source = new FolderBrowserDialog();
+            if (source.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SourceFolder = source.SelectedPath;
+                textBox1.Text = SourceFolder.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog destination = new FolderBrowserDialog();
+            if (destination.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DestinationFolder = destination.SelectedPath;
+                textBox2.Text = DestinationFolder.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(SourceFolder);
+            foreach (string file in files)
+            {
+                string DestinationFile = DestinationFolder + "\\" + Path.GetFileName(file);
+                File.Copy(file, DestinationFile);
+            }
+        }
+    }
+}
